9e148257d4e46ce84484954800e7450f34f82bb2
Ajay Patel <ajay61081@outlook.com>
2022-09-24 11:53:59 +0530

    CR-112 - chnage the  comments

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -157,7 +157,7 @@ namespace WebApplication1.Controllers
         {
             try
             {
-                //Save Profile - second change
+                //Save Profile
                 var httpRequest = Request.Form;
                 var postFile = httpRequest.Files[0];
                 string filename = postFile.FileName;
